ba1a7765b59db6852ea1674cd942dd20b0451cd9
Nello Casolla <nellocasolla446@gmail.com>
2023-03-03 15:59:36 +0100

    second-commit

---
+++ assets/js/scroll.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 1) {
+      $(".navbar").css("background-color", "white");
+      $(".btn1").css("background-color", "green");
+    } else {
+      $(".navbar").css("background-color", "transparent");
+      $(".btn1").css("background-color", "transparent");
+    }
+  });
+  
